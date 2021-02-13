21de8636b41700224c9769ebe58b8c985c03d3c5
ScoJo55 <75748384+ScoJo55@users.noreply.github.com>
2021-02-13 14:52:54 -0600

    updated files

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+api_key = "760c75b11d50562f9a5215b49803480f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBlOylEYuVo9lZ9QdfR7F7bbcZlOEHt7vQ"
